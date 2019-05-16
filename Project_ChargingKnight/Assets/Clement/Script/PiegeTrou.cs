048c34702fbb3a248ea4b8b63c7c010d6bf8cdd9
ClementColmerauer <clement.colmerauer@gmail.com>
2019-05-16 16:55:20 +0200

    Jedoispull

---
+++ Project_ChargingKnight/Assets/Clement/Script/PiegeTrou.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PiègeTrou : MonoBehaviour {
+
+    GameObject player;
+    public float degats;
+    Vector3 velocity;
+    Vector3 vecWithPl;
+    float playerX;
+    float playerZ;
+    float playerY;
+    float boundMinX;
+    float boundMaxX;
+    float boundMinZ;
+    float boundMaxZ;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.Find("Player");
+        float boundMinX = gameObject.GetComponent<Collider>().bounds.min.x;
+        float boundMaxX = gameObject.GetComponent<Collider>().bounds.max.x;
+        float boundMinZ = gameObject.GetComponent<Collider>().bounds.min.z;
+        float boundMaxZ = gameObject.GetComponent<Collider>().bounds.max.z;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (player.CompareTag("Player") == true)
+        {
+
+            velocity = player.GetComponent<Rigidbody>().velocity;
+            playerZ = player.transform.position.z;
+            playerY = player.transform.position.y;
+            playerX  = player.transform.position.x;
+            player.GetComponent<PlayerLifeManager>().life = player.GetComponent<PlayerLifeManager>().life - degats;
+            
+            if(velocity.x + Vector3.zero.x < 0)
+            {
+                player.transform.position.Set(boundMaxX + 1, playerY, playerZ);
+                //vecWithPl = new Vector3(-1, playerY, playerZ);
+                player.transform.LookAt(player.transform.position + Vector3.forward);
+            }
+        }
+    }
+}
---
+++ Project_ChargingKnight/Assets/Clement/Script/PlayerLifeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLifeManager : MonoBehaviour {
+
+    public float life;
+    int degats;
+    public Slider barVie;
+    float sliderValue;
+
+	// Use this for initialization
+	void Start () {
+     barVie.maxValue = life;
+     sliderValue = life;
+     barVie.value = sliderValue;
+        System.Math.Round(life, 2);
+            }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnHit()
+    {
+            //degats = collision.gameObject.GetComponent<Script>.degats;
+            sliderValue = barVie.value;
+            life = life - degats;
+            sliderValue.Equals(life);    
+    }
+}
