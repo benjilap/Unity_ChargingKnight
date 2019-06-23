30c15b33c4e2678336476d59dd8aaeaa02af1381
benjilap <benjilap2@yahoo.fr>
2019-06-23 13:49:49 +0200

    up ui pl

--- Project_ChargingKnight/Assets/Benji/Scripts/GameScripts/GameManager.cs
+++ Project_ChargingKnight/Assets/Benji/Scripts/GameScripts/GameManager.cs
@@ -6,7 +6,7 @@ public static class GameManager
 {
 
     public static int defaultPlayerNum = 1;
-    public static int playersNmbrs = 2;
+    public static int playersNmbrs = 1;
     public static float playersHeight = 0.25f;
     public static Color P1Color = Color.red;
     public static Color P2Color = Color.blue;
--- Project_ChargingKnight/Assets/Benji/Scripts/PlayerScripts/PlayerUIScript.cs
+++ Project_ChargingKnight/Assets/Benji/Scripts/PlayerScripts/PlayerUIScript.cs
@@ -8,10 +8,16 @@ public class PlayerUIScript : MonoBehaviour {
     [HideInInspector]
     public PlayerClass myPlayer;
 
+    LifeGlobalScript lifeScript;
+    KnightClass characClass;
+
+    Text lifeText;
     Scrollbar lifeBar;
     Scrollbar burstRecover;
     Scrollbar bashRecover;
     Scrollbar slashRecover;
+
+    public float[] saveTime = new float[3];
 
     // Use this for initialization
     void Start () {
@@ -20,7 +26,8 @@ public class PlayerUIScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        UpdateLifeVisual();
+        CheckSkillUse();
 	}
 
     void InitUI()
@@ -28,11 +35,70 @@ public class PlayerUIScript : MonoBehaviour {
         if (myPlayer.playerNum == 2) {
             this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
             this.transform.Find("PlayerLifeBar/PlayerLifePoint").GetComponent<RectTransform>().localScale = this.transform.localScale;
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    this.transform.Find("SkillPanel").GetChild(i).Find("SkillText").GetComponent<RectTransform>().localScale = this.transform.localScale;
+            //}
         }
+        lifeScript = myPlayer.GetComponent<LifeGlobalScript>();
+        characClass = myPlayer.GetComponent<KnightClass>();
 
+        lifeText = this.transform.Find("PlayerLifeBar/PlayerLifePoint").GetComponent<Text>();
         lifeBar = this.transform.Find("PlayerLifeBar/Scrollbar").GetComponent<Scrollbar>();
         burstRecover = this.transform.Find("SkillPanel/BurstModeRecover/Scrollbar").GetComponent<Scrollbar>();
         bashRecover = this.transform.Find("SkillPanel/BashingShieldRecover/Scrollbar").GetComponent<Scrollbar>();
         slashRecover = this.transform.Find("SkillPanel/RazorSlashRecover/Scrollbar").GetComponent<Scrollbar>();
+
+
+    }
+
+    void UpdateLifeVisual()
+    {
+        lifeBar.size = lifeScript.lifeValue/100;
+        lifeText.text = lifeScript.lifeValue.ToString();
+    }
+
+
+    void CheckSkillUse()
+    {
+        //BURSTMODE
+        if (SaveTimeRecover(characClass.burstModeRecover) != 0)
+        {
+            saveTime[0] = SaveTimeRecover(characClass.burstModeRecover);
+        }
+        burstRecover.size = RecoverSkill(saveTime[0], characClass.burstModeRecoverTime);
+
+        //BASHINGSHIELD
+        if (SaveTimeRecover(characClass.bashingShieldRecover) != 0)
+        {
+            saveTime[1] = SaveTimeRecover(characClass.bashingShieldRecover);
+        }
+        bashRecover.size = RecoverSkill(saveTime[1], characClass.bashingShieldRecoverTime);
+
+        //RAZORSLASH
+        if (SaveTimeRecover(characClass.razorSlashRecover) != 0)
+        {
+            saveTime[2] = SaveTimeRecover(characClass.razorSlashRecover);
+        }
+        slashRecover.size = RecoverSkill(saveTime[2], characClass.razorSlashRecoverTime);
+        
+    }
+
+    float SaveTimeRecover(bool capNoUsed)
+    {
+
+        if (!capNoUsed)
+        {
+            return Time.time;
+        }
+        else return 0;
+    }
+
+    float RecoverSkill(float savedTime, float timeToRecover)
+    {
+
+            float recoverTimer = Time.time - savedTime;
+            return Mathf.Lerp(0, 1, (recoverTimer / timeToRecover) * 100);
+
     }
 }
